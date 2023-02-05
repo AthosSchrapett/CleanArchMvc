83001d9c6c166cd671659745a2d0ff10fc63d85c
Athos Schrapett <athosschrapett@outlook.com>
2023-02-05 12:33:31 -0300

    feat: aulas iniciais.

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid value.");
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 charecters.");
+
+            Name = name;
+        }
+
+    }
+}
---
+++ CleanArchMvc.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchMvc.WebUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
