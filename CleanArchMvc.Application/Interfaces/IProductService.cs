c17aa7c8611d6ccb06cd3e047924d305bdf34e7c
Athos Schrapett <athosschrapett@outlook.com>
2023-02-08 10:17:54 -0300

    feat: 50. Implementando o serviço para Product - ProductService

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProductsAsync();
+        Task<ProductDTO> GetByIdAsync(int? id);
+        Task<ProductDTO> GetProductCategoryAsync(int? id);
+        Task AddAsync(ProductDTO productDTO);
+        Task UpdateAsync(ProductDTO productDTO);
+        Task RemoveAsync(int? id);
+    }
+}
---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentNullException(nameof(productRepository));
+
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateAsync(productEntity);
+        }
+
+        public async Task<ProductDTO> GetByIdAsync(int? id)
+        {
+            var productEntity = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategoryAsync(int? id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProductsAsync()
+        {
+            var productEntity = await _productRepository.GetProductsAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productEntity);
+        }
+
+        public async Task RemoveAsync(int? id)
+        {
+            var productEntity = _productRepository.GetByIdAsync(id).Result;
+            await _productRepository.DeleteAsync(productEntity);
+        }
+
+        public async Task UpdateAsync(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.UpdateAsync(productEntity);
+        }
+    }
+}
