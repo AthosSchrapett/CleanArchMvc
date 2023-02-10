d2408c511d0f7bb9baacfb98d9d80292286ee202
Athos Schrapett <athosschrapett@outlook.com>
2023-02-10 09:26:45 -0300

    feat: 57. CQRS - Implementando os Handlers para Commands e Queries

---
+++ CleanArchMvc.Application/Products/Handlers/GetProductsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Application.Products.Queries;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductsAsync();
+        }
+    }
+}
