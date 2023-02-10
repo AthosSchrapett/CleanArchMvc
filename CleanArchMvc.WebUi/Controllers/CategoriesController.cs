2d928e5f4ccada63e9ecf0658d1b0d9301867db1
Athos Schrapett <athosschrapett@outlook.com>
2023-02-10 20:33:07 -0300

    feat: 64. Criando uma nova categoria

--- CleanArchMvc.WebUi/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUi/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -18,6 +19,23 @@ namespace CleanArchMvc.WebUi.Controllers
         {
             var categories = await _categoryService.GetCategoriesAsync();
             return View(categories);
+        }
+
+        [HttpGet()]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.AddAsync(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
         }
     }
 }
