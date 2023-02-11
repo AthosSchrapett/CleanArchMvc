6d3e2c71a537bdc748912bf457e0a1475bdc9c7f
Athos Schrapett <athosschrapett@outlook.com>
2023-02-10 21:53:40 -0300

    feat: 65. Editar uma categoria

--- CleanArchMvc.WebUi/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUi/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUi.Controllers
@@ -27,6 +28,7 @@ namespace CleanArchMvc.WebUi.Controllers
             return View();
         }
 
+        [HttpPost]
         public async Task<IActionResult> Create(CategoryDTO category)
         {
             if (ModelState.IsValid)
@@ -36,6 +38,36 @@ namespace CleanArchMvc.WebUi.Controllers
             }
 
             return View(category);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null) return NotFound();
+
+            var categoryDTO = await _categoryService.GetByIdAsync(id);
+
+            if(categoryDTO == null) return NotFound();
+
+            return View(categoryDTO);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.UpdateAsync(categoryDTO);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDTO);
         }
     }
 }
