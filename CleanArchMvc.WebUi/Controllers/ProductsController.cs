623004d27101df231fa79b1458ccf19eaa736593
Athos Schrapett <athosschrapett@outlook.com>
2023-02-11 13:55:24 -0300

    feat: 71. Exibir detalhes de um produto

--- CleanArchMvc.WebUi/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUi/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
 ﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUi.Controllers
@@ -10,11 +12,17 @@ namespace CleanArchMvc.WebUi.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _enviroment;
 
-        public ProductsController(IProductService productService, ICategoryService categoryService)
+        public ProductsController(
+            IProductService productService, 
+            ICategoryService categoryService,
+            IWebHostEnvironment environment
+        )
         {
             _productService = productService;
             _categoryService = categoryService;
+            _enviroment = environment;
         }
 
         [HttpGet]
@@ -86,8 +94,26 @@ namespace CleanArchMvc.WebUi.Controllers
         [HttpPost, ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(int? id)
         {
-            await _productService.RemoveAsync(id.Value);
+            await _productService.RemoveAsync(id);
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productDTO = await _productService.GetByIdAsync(id);
+
+            if (productDTO == null) return NotFound();
+
+            var wwwroot = _enviroment.WebRootPath;
+            var image = Path.Combine(wwwroot, $"images\\{productDTO.Image}");
+            var exists = System.IO.File.Exists(image);
+
+            ViewBag.ImageExist = exists;
+
+            return View(productDTO);
         }
     }
 }
