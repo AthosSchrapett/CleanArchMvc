fb1d57b7b07d5aded0ab35f36707985a62917dbd
Athos Schrapett <athosschrapett@outlook.com>
2023-02-12 12:42:37 -0300

    feat: 77. Implementando a interface IAutenticate

---
+++ CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(string email, string password);
+        Task<bool> RegisterUserAsync(string email, string password);
+        Task LogoutAsync();
+    }
+}
