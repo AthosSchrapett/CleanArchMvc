61acdb19b5b71830d6b156cfd4c377a665a7b1c1
Athos Schrapett <athosschrapett@outlook.com>
2023-02-13 10:49:53 -0300

    feat: 89. HettpGet - Retornando todas as Categorias e 90. HttpGet{id} - Retornando uma Categoria pelo seu Id

---
+++ CleanArchMvc.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+            
+            if(categories == null) 
+                return NotFound("Categories not found.");
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            if (category == null)
+                return NotFound("Category not found.");
+
+            return Ok(category);
+        }
+    }
+}
