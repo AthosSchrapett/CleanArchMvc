8590077256cd90708bf9ac5794638aee627ea83d
Athos Schrapett <athosschrapett@outlook.com>
2023-02-13 20:44:30 -0300

    feat: 102. Ativando a autenticação e validando o token JWT

--- CleanArchMvc.Api/Startup.cs
+++ CleanArchMvc.Api/Startup.cs
@@ -28,6 +28,8 @@ namespace CleanArchMvc.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructureAPI(Configuration);
+            services.AddInfrastructureJWT(Configuration);
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
