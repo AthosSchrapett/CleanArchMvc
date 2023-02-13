310c1634703437b067eced3d175c2c2b0283bb3d
Athos Schrapett <athosschrapett@outlook.com>
2023-02-13 12:20:29 -0300

    feat: 98. Configurando as informações do token e os DTOs usados

---
+++ CleanArchMvc.Api/Controllers/TokenController.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchMvc.Api.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authentication;
+
+        public TokenController(IAuthenticate authentication)
+        {
+            _authentication = authentication ??
+                throw new ArgumentNullException(nameof(authentication));
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.AuthenticateAsync(userInfo.Email, userInfo.Password);
+
+            if (result)
+                return Ok($"User {userInfo.Email} login successfully");
+                //return GenerateToken(userInfo);
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt");
+                return BadRequest(ModelState);
+            }
+
+        }
+    }
+}
