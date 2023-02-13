ce20b6491436f5c293a0edcde6eb2aced695085d
Athos Schrapett <athosschrapett@outlook.com>
2023-02-13 12:10:31 -0300

    feat: 98. Configurando as informações do token e os DTOs usados

---
+++ CleanArchMvc.Api/Models/UserToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CleanArchMvc.Api.Models
+{
+    public class UserToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
