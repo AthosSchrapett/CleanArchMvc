de6b8c8f0a434b26e82d1364201e5b1ee9b6c487
Athos Schrapett <athosschrapett@outlook.com>
2023-02-13 11:18:16 -0300

    feat: 94. Implementando o controlador ProductsController

---
+++ CleanArchMvc.Api/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
